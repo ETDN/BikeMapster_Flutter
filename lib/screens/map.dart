e8da4d7fb53ec7e8890bdbda24dce2ae6ea1a9df
Gaétan Mottet <gaetan.mottet@students.hevs.ch>
2022-11-30 22:17:46 +0100

    Switch pc_Gaétan

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,49 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'drawer_nav.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  List<Marker> myMarkers = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: const DrawerNav(),
         appBar: AppBar(
-          title: Text(
-            'Map',
-            style: GoogleFonts.bebasNeue(
-                fontSize: 22,
-                fontWeight: FontWeight.w300,
-                color: Color.fromRGBO(0, 181, 107, 1)),
-          ),
-          iconTheme: IconThemeData(color: Color.fromRGBO(0, 181, 107, 1)),
+          backgroundColor: Colors.green,
           centerTitle: true,
-          backgroundColor: Colors.white,
+          title: Text('Bike Mapster'),
         ),
-        body: FlutterMap(
-          options: MapOptions(
-            center: LatLng(46.2293518, 7.3620487),
-            zoom: 13.0,
-          ),
-          layers: [
-            TileLayerOptions(
-                urlTemplate:
-                    "https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.swissimage/default/current/3857/{z}/{x}/{y}.jpeg",
-                subdomains: ['a', 'b', 'c']),
-            MarkerLayerOptions(
-              markers: [
-                Marker(
-                  width: 80.0,
-                  height: 80.0,
-                  point: LatLng(46.2293518, 7.3620487),
-                  builder: (ctx) => Container(),
+        body: Center(
+          child: Container(
+            // child: Column(
+            //   children: [
+            //     Flexible(
+            child: FlutterMap(
+              options: MapOptions(
+                center: LatLng(46.2293518, 7.3620487),
+                zoom: 13.0,
+                onTap: (tapPosition, point) => _handleTap(point),
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate:
+                      "https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.swissimage/default/current/3857/{z}/{x}/{y}.jpeg",
+                ),
+                MarkerLayer(
+                  markers: myMarkers,
                 ),
               ],
             ),
-          ],
+            //     ),
+            //   ],
+            // ),
+          ),
         ));
+  }
+
+  _handleTap(LatLng tappedPoint) {
+    setState(() {
+      myMarkers = [];
+      myMarkers.add(
+        Marker(
+          point: tappedPoint,
+          builder: (context) => Icon(
+            Icons.location_on_sharp,
+            color: Colors.green,
+            size: 50,
+          ),
+          key: Key(tappedPoint.toString()),
+        ),
+      );
+    });
   }
 }
