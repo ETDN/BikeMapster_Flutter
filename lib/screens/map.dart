39e2c6113888dfe1d702a3c8d40e799ea65175df
Gaétan Mottet <gaetan.mottet@students.hevs.ch>
2022-12-01 22:19:14 +0100

    Drawing route works but the update of display needs to be fix. For the moment it is done with a time delay.
    Easier way to fix:
    Implementing it in dedicated method linked to a button "add route" or "draw route".

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -3,6 +3,9 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'navbar/drawer_nav.dart';
+import 'package:routing_client_dart/routing_client_dart.dart';
+
+import 'networkHelper_map.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -12,8 +15,13 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  //for holding starting and destination points
   List<Marker> myMarkers = [];
+  //for holding all points needed to draw the route
+  List<LatLng> polyPoints = [];
+  //for knowing if starting point or destination point is selected
   var currentIndex = 0;
+  var data;
 
   @override
   Widget build(BuildContext context) {
@@ -49,6 +57,17 @@ class _MapPageState extends State<MapPage> {
               ),
               MarkerLayer(
                 markers: myMarkers,
+              ),
+              PolylineLayer(
+                polylineCulling: false,
+                polylines: [
+                  Polyline(
+                    points: polyPoints,
+                    color: Colors.blue,
+                    borderColor: Colors.blueAccent,
+                    borderStrokeWidth: 7.0,
+                  ),
+                ],
               ),
               // FloatingActionButton(
               //   onPressed: _changeMapType,
@@ -100,7 +119,7 @@ class _MapPageState extends State<MapPage> {
               color: Colors.redAccent,
               size: 50,
             ),
-            key: Key(tappedPoint.toString()),
+            key: Key("start"),
           ),
         );
         print(tappedPoint.toString());
@@ -109,6 +128,7 @@ class _MapPageState extends State<MapPage> {
         if (myMarkers.length > 1) {
           myMarkers.removeAt(currentIndex);
         }
+        polyPoints.clear();
         myMarkers.insert(
           currentIndex,
           Marker(
@@ -118,10 +138,78 @@ class _MapPageState extends State<MapPage> {
               color: Colors.redAccent,
               size: 50,
             ),
-            key: Key(tappedPoint.toString()),
+            key: Key("end"),
           ),
         );
+        setState(() {
+          getJsonData();
+        });
       }
     });
   }
+
+  _getTripInformation() async {
+    //drawing route using ORSM package
+    if (myMarkers.length != 2) {
+      return;
+    }
+    print("Trying to print a road");
+    var latStart = myMarkers.first.point.latitude;
+    var lngStart = myMarkers.first.point.longitude;
+    var latEnd = myMarkers.last.point.latitude;
+    var lngEnd = myMarkers.last.point.longitude;
+
+    List<LngLat> waypoints = [
+      LngLat(lng: lngStart, lat: latStart),
+      LngLat(lng: lngEnd, lat: latEnd)
+    ];
+    final manager = OSRMManager();
+    final road = await manager.getTrip(
+        waypoints: waypoints,
+        roundTrip: true,
+        destination: DestinationGeoPointOption.last,
+        source: SourceGeoPointOption.first,
+        geometry: Geometries.geojson,
+        steps: true,
+        languageCode: "en");
+
+    // print(road.polyline);
+  }
+
+  /*largely inspired by Arafat Rohan's method, found here: 
+  https://medium.com/@rohanarafat86/drawing-route-direction-in-flutter-using-openrouteservice-api-and-google-maps-in-flutter-4431a2989dd5
+  Git repository : https://github.com/ArafatRohan93/flutter-polyline-demo/blob/master/lib/main.dart
+  */
+  void getJsonData() async {
+    // Create an instance of Class NetworkHelper which uses http package
+    // for requesting data to the server and receiving response as JSON format
+
+    NetworkHelper network = NetworkHelper(
+      startLat: myMarkers.first.point.latitude,
+      startLng: myMarkers.first.point.longitude,
+      endLat: myMarkers.last.point.latitude,
+      endLng: myMarkers.last.point.longitude,
+    );
+
+    try {
+      // getData() returns a json Decoded data
+      data = await network.getData();
+
+      // We can reach to our desired JSON data manually as following
+      LineString ls =
+          LineString(data['features'][0]['geometry']['coordinates']);
+
+      for (int i = 0; i < ls.lineString.length; i++) {
+        polyPoints.add(LatLng(ls.lineString[i][1], ls.lineString[i][0]));
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+}
+
+//Create a new class to hold the Co-ordinates we have received from the response data
+class LineString {
+  LineString(this.lineString);
+  List<dynamic> lineString;
 }
