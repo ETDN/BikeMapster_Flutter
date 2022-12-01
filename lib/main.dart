05b303601ed12eee1e775f0bf8c146f4cf80796d
DnPint <daniel.pintodem@hes-so.ch>
2022-12-01 15:01:24 +0100

    get routes, post routes

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(new MaterialApp(
     home: SplashScreen(),
   ));
