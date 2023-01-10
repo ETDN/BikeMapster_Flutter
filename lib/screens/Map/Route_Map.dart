43fdd29368b2292959cb2663db192725e9b75f71
DnPint <daniel.pintodem@hes-so.ch>
2023-01-10 16:18:54 +0100

    added position of user

--- lib/screens/Map/Route_Map.dart
+++ lib/screens/Map/Route_Map.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart';
 import 'package:latlong2/latlong.dart';
@@ -15,11 +17,11 @@ import 'map.dart';
 import 'map_page.dart';
 
 class RouteMap extends StatefulWidget {
-  var routeId;
-
   RouteMap(String routeId, {super.key}) {
     this.routeId = routeId;
   }
+
+  var routeId;
 
   @override
   State<RouteMap> createState() => _RouteMapState(routeId);
@@ -28,6 +30,20 @@ class RouteMap extends StatefulWidget {
 //This class will be used to display the map of the route and the user's location
 class _RouteMapState extends State<RouteMap> {
   var routeId;
+
+  String _latitude = "";
+  String _longitude = "";
+  String _altitude = "";
+  String _speed = "";
+
+  var userLocationAuthorized;
+
+  var currentLocation;
+
+  var startLocation;
+
+  var destination;
+
   _RouteMapState(String routeId) {
     this.routeId = routeId;
   }
@@ -39,6 +55,7 @@ class _RouteMapState extends State<RouteMap> {
 
   @override
   Widget build(BuildContext context) {
+    //_updatePosition();
     return FutureBuilder(
         future:
             FirebaseFirestore.instance.collection('Routes').doc(routeId).get(),
@@ -100,7 +117,7 @@ class _RouteMapState extends State<RouteMap> {
                         );
                       } else {
                         return Container(
-                          child: Text("Loading"),
+                          child: Center(child: CircularProgressIndicator()),
                         );
                       }
                     }));
@@ -129,6 +146,85 @@ class _RouteMapState extends State<RouteMap> {
         });
   }
 
+  Future<void> _updatePosition() async {
+    Position pos = await _determinePosition();
+
+    //setState(() {
+    _latitude = pos.latitude.toString();
+    _longitude = pos.longitude.toString();
+    _altitude = pos.altitude.toString();
+    _speed = pos.speed.toString();
+    //});
+
+    if (userLocationAuthorized) {
+      currentLocation = pos;
+
+      Marker userLocationMarker = Marker(
+        width: 30.0,
+        height: 30.0,
+        point: LatLng(currentLocation.latitude, currentLocation.longitude),
+        builder: (ctx) => Container(
+          child: CircleAvatar(
+            backgroundColor: Colors.blue,
+            radius: 2.0,
+            child: Icon(
+              Icons.person,
+              color: Colors.white54,
+              size: 30.0,
+            ),
+          ),
+        ),
+      );
+
+      myMarkers["userLocation"] = userLocationMarker;
+    }
+  }
+
+//GPS - user location - code from https://pub.dev/packages/geolocator
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time user could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // the App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // Modify control boolean
+    userLocationAuthorized = true;
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator
+        .getCurrentPosition(); //.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  }
+
   _handleTap(LatLng tappedPoint) {
     print(tappedPoint);
   }
@@ -144,6 +240,7 @@ class _RouteMapState extends State<RouteMap> {
     );
 
     try {
+      await _updatePosition();
       // getData() returns a json Decoded data
       var data = await network.getData();
 
